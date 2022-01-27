a1b8e2e9c3d4aa9f23d57f0d6041580e53e57f46
OgheneTega <oshinowojoshua@gmail.com>
2022-01-27 16:46:59 +0100

    clima final

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,28 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '35d2a1d487c221b5959140b6f1ddf3fa';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String typedName) async {
+    var url = '$openWeatherMapURL?q=$typedName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = url != null ? NetworkHelper(url) : "";
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLoctionWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    var url =
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = url != null ? NetworkHelper(url) : "";
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
